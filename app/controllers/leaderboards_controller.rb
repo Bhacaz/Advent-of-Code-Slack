464d4912fad89092e04b76bd4bc273794f6c9494
Jean-Francis Bastien <bhacaz@gmail.com>
2020-11-06 19:39:26 -0500

    View works

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,4 +1,5 @@
 class LeaderboardsController < ApplicationController
+  INTERVALS = [['15 minutes', 15], ['30 minutes', 2], ['1 hour', 60], ['2 hours', 120], ['4 hours', 240], ['6 hours', 360], ['24 hours', 1440]].freeze
   def show
   end
 
@@ -18,6 +19,7 @@ class LeaderboardsController < ApplicationController
   end
 
   def show_post_configs
+    @intervals = INTERVALS
     @leaderboard = Leaderboard.find(params[:id])
     @post_config = @leaderboard.post_config
   end
@@ -25,7 +27,6 @@ class LeaderboardsController < ApplicationController
   def update_post_configs
     post_config = PostConfig.find_or_initialize_by(leaderboard_id: params[:id])
     post_config.display_other = false unless post_configs_params.key?(:display_other)
-    p post_configs_params
     post_config.assign_attributes(post_configs_params)
     post_config.save!
     redirect_to show_post_configs_leaderboards_path(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'post_configs/new'
-  get 'post_configs/show'
-  get 'post_configs/delete'
-  get 'post_configs/update'
-  get 'post_configs/edit'
-  get 'post_configs/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'leaderboards#new'
-  resources :leaderboards
+  resources :leaderboards, only: %i[create new]
   get 'leaderboards/:id/post_configs' => 'leaderboards#show_post_configs', as: :show_post_configs_leaderboards
   match 'leaderboards/:id/post_configs' => 'leaderboards#update_post_configs', as: :update_post_configs_leaderboards, via: [:patch, :post]
 end
