fad1e6fb72221b717a9226623dd6f632248141e5
Jean-Francis Bastien <bhacaz@gmail.com>
2020-11-08 20:56:32 -0500

    Use leaderboard id has id

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,6 +1,4 @@
 class LeaderboardsController < ApplicationController
-  def show
-  end
 
   def new
     @leaderboard = Leaderboard.new
@@ -9,28 +7,23 @@ class LeaderboardsController < ApplicationController
   def delete
   end
 
-  def update
-  end
-
   def create
     @leaderboard = Leaderboard.find_or_create_by!(leaderboard_params)
-    redirect_to show_post_configs_leaderboards_path(@leaderboard)
+    redirect_to show_post_configs_leaderboards_path(@leaderboard.leaderboard_id)
   end
 
   def show_post_configs
-    @leaderboard = Leaderboard.find(params[:id])
+    @leaderboard = Leaderboard.find_by(leaderboard_id: params[:id])
     @post_config = @leaderboard.post_config || PostConfig.new(leaderboard: @leaderboard)
   end
 
   def update_post_configs
-    post_config = PostConfig.find_or_initialize_by(leaderboard_id: params[:id])
+    @leaderboard = Leaderboard.find_by(leaderboard_id: params[:id])
+    post_config = PostConfig.find_or_initialize_by(leaderboard_id: @leaderboard.id)
     post_config.display_other = false unless post_configs_params.key?(:display_other)
     post_config.assign_attributes(post_configs_params)
     post_config.save!
     redirect_to show_post_configs_leaderboards_path(params[:id])
-  end
-
-  def edit
   end
 
   private
