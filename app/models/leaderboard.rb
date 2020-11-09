0de59bfb8f777657f40a8b3eb9c2fad6c9967453
Jean-Francis Bastien <bhacaz@gmail.com>
2020-11-08 21:56:02 -0500

    leaderboard_id unique

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -5,7 +5,7 @@ class Leaderboard < ApplicationRecord
   has_many :members
   has_one :post_config
 
-  validates :leaderboard_id, presence: true
+  validates :leaderboard_id, presence: true, uniqueness: true
 
   def fetch_leaderboard_data
     headers = { cookie: "session=#{token}" }
--- db/migrate/20201101191808_create_leaderboards.rb
+++ db/migrate/20201101191808_create_leaderboards.rb
@@ -1,7 +1,7 @@
 class CreateLeaderboards < ActiveRecord::Migration[6.0]
   def change
     create_table :leaderboards do |t|
-      t.integer :leaderboard_id, null: false
+      t.integer :leaderboard_id, null: false, index: { unique: true }
       t.string :token, null: false
 
       t.timestamps
