7894a6ae3b5025e9803d8b59ba065981ba36addf
Jean-Francis Bastien <bhacaz@gmail.com>
2020-11-19 20:09:27 -0500

    UI not bad

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  require 'sidekiq/testing'
-  Sidekiq::Testing.inline!
+  # require 'sidekiq/testing'
+  # Sidekiq::Testing.inline!
 end
