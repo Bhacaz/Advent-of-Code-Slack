f7e3459ccc3dc31d5206f366d285de1d354c67ed
Jean-Francis Bastien <bhacaz@gmail.com>
2020-12-01 09:51:55 -0500

    Handle anonymous user

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,8 @@
 class Member < ApplicationRecord
   belongs_to :leaderboard
   has_many :scores, class_name: 'MemberScore', dependent: :destroy
+
+  def name
+    super || "(anonymous user ##{member_id})"
+  end
 end
