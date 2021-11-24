53de72400128e1b130fc73bc6b4c5ec29026bd0d
Jean-Francis Bastien <bhacaz@gmail.com>
2021-11-24 08:53:51 -0500

    Add distinct for bot

--- app/jobs/advent_slack_bot_job.rb
+++ app/jobs/advent_slack_bot_job.rb
@@ -2,7 +2,7 @@ class AdventSlackBotJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    PostConfig.all.pluck(:leaderboard_id).each do |leaderboard_id|
+    PostConfig.all.distinct.pluck(:leaderboard_id).each do |leaderboard_id|
       BotJob.perform_later(leaderboard_id)
     end
   end
